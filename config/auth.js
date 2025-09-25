664b58fbe6feb8156f132468526d73de5603954b
Ira Rickman <irarickman@gmail.com>
2025-09-24 21:47:24 -0700

    force: trigger deployment for password update

--- config/auth.js
+++ config/auth.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // Default password: "hollywoodhandshake"
+  // Default password: "hollywoodhandshake" - updated
   AUTH_PASSWORD_HASH: '$2b$10$KTIKFAP1dOpdkx0WFFoXsOvTw/U87NLUHSCu.ymwAv1W4oTnLYOcC',
   IRON_SESSION_PASSWORD: 'gbbo-fantasy-league-secret-key-32chars',
   IRON_SESSION_COOKIE_NAME: 'gbbo_session',
