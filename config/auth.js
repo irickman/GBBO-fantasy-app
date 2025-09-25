b696ec85c15b23784f4df3c33fa1a18af72afb5b
Ira Rickman <irarickman@gmail.com>
2025-09-24 21:48:12 -0700

    refactor: update auth config structure

--- config/auth.js
+++ config/auth.js
@@ -1,3 +1,4 @@
+// Authentication configuration
 module.exports = {
   // Default password: "hollywoodhandshake" - updated
   AUTH_PASSWORD_HASH: '$2b$10$KTIKFAP1dOpdkx0WFFoXsOvTw/U87NLUHSCu.ymwAv1W4oTnLYOcC',
