44d6e76c3126108b4a2b8dd19d3420561928fb11
Ira Rickman <irarickman@gmail.com>
2025-09-24 21:46:43 -0700

    fix: update password hash

--- config/auth.js
+++ config/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Default password: "hollywoodhandshake"
-  AUTH_PASSWORD_HASH: '$2b$10$lv0stE8EG5AUD3bxleY.Ne7P.XkSGqWDcbrpgWdZ2oDfLLk9u2nDy',
+  AUTH_PASSWORD_HASH: '$2b$10$KTIKFAP1dOpdkx0WFFoXsOvTw/U87NLUHSCu.ymwAv1W4oTnLYOcC',
   IRON_SESSION_PASSWORD: 'gbbo-fantasy-league-secret-key-32chars',
   IRON_SESSION_COOKIE_NAME: 'gbbo_session',
   NEXTAUTH_URL: 'http://localhost:3000',
