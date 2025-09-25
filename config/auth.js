c18411a61b3ae4bc890e8271550c1afe7d864cea
Ira Rickman <irarickman@gmail.com>
2025-09-24 21:45:58 -0700

    update: change application password

--- config/auth.js
+++ config/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // Default password: "yummybread"
-  AUTH_PASSWORD_HASH: '$2b$10$R15IXnRdHLIZYEx9eZ0UgOfufPCU7kG3kvQz4GfSaUJHaWm4EqPQG',
+  // Default password: "hollywoodhandshake"
+  AUTH_PASSWORD_HASH: '$2b$10$lv0stE8EG5AUD3bxleY.Ne7P.XkSGqWDcbrpgWdZ2oDfLLk9u2nDy',
   IRON_SESSION_PASSWORD: 'gbbo-fantasy-league-secret-key-32chars',
   IRON_SESSION_COOKIE_NAME: 'gbbo_session',
   NEXTAUTH_URL: 'http://localhost:3000',
