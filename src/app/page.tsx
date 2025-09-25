c3c157e47fe74e399d0bc3a1074acda375486024
Ira Rickman <irarickman@gmail.com>
2025-09-24 21:43:20 -0700

    force: trigger deployment with comment update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  // Redirect to login page
+  // Redirect to login page - users must authenticate first
   redirect('/login')
 }
